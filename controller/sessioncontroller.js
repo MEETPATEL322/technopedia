35318848916c5420b7750a1f66843fa1fe72db80
MEETPATEL322 <meet1patel0@gmail.com>
2022-02-17 15:50:47 +0530

    session controller

--- controller/sessioncontroller.js
+++ controller/sessioncontroller.js
@@ -1,11 +1,28 @@
-function login(req,res){
-    res.write("login")
-    res.end()
-}
-function signup(req,res){
+const fs = require("fs")
 
-    res.write("signup")
+
+
+function login(req, res) {
+    let loginhtml = fs.readFileSync("./views/login.html")
+    res.write(loginhtml)
     res.end()
 }
-module.exports.login=login
-module.exports.signup=signup
+function signup(req, res) {
+    let signuphtml = fs.readFileSync("./views/signup.html")
+    res.write(signuphtml)
+    res.end()
+}
+function saveuser(req, res) {
+    console.log(req.body)
+    res.json({
+
+        msg: "done dona don",
+        status: 200,
+        data: req.body
+    })
+}
+
+    module.exports.login = login
+    module.exports.signup = signup
+    module.exports.saveuser = saveuser
+    
