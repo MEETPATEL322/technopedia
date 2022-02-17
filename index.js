1358c38984ed974c096d8be0d608408e05711f76
MEETPATEL322 <meet1patel0@gmail.com>
2022-02-17 18:12:27 +0530

    rolladd and db conection

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const mongoose=require("mongoose")
+
 const sessioncontroller=require("./controller/sessioncontroller")
+const rolecontroller=require("./controller/role-controller")
 const app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+ mongoose.connect('mongodb://localhost:27017/technopedia',function(err){
+     if(err){
+         console.log("db connection ids failed");
+         console.log(err);
+     }
+     else{
+         console.log("db connected")
+     }
+ })
+
 
 app.get("/",function(req,res){
 
@@ -22,7 +35,7 @@ app.get("/signup",function(req,res){
 app.get("/login",sessioncontroller.login)
 app.get("/signup",sessioncontroller.signup)
 app.post("/saveuser",sessioncontroller.saveuser)
-
+app.post("/rolls",rolecontroller.addrole)
 app.listen(3000,function(){
     console.log("server is started on 3000");
 })
