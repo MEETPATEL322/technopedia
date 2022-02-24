cff6b6643cc305be388344350d0eacd5bdc234e2
MEETPATEL322 <meet1patel0@gmail.com>
2022-02-24 10:54:06 +0530

    role addedin database

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const rolecontroller=require("./controller/role-controller")
 const app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+//database
  mongoose.connect('mongodb://localhost:27017/technopedia',function(err){
      if(err){
          console.log("db connection ids failed");
@@ -32,10 +34,16 @@ app.get("/signup",function(req,res){
     res.write("signup")
     res.end();
 })*/
+
+//urls
 app.get("/login",sessioncontroller.login)
 app.get("/signup",sessioncontroller.signup)
 app.post("/saveuser",sessioncontroller.saveuser)
-app.post("/rolls",rolecontroller.addrole)
+app.post("/roles",rolecontroller.addrole)
+
+
+
+//server
 app.listen(3000,function(){
     console.log("server is started on 3000");
 })
